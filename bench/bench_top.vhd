10e6054d8f762b76b7faf5db20e6be81da94ad23
aureliengoux <goux.aurelien@gmail.com>
2017-12-22 17:50:06 +0100

    fit  test bench with strat and done signal

--- bench/bench_top.vhd
+++ bench/bench_top.vhd
@@ -17,20 +17,24 @@ architecture tb of tb_top is
   	port (
     	clk : in std_logic;
 			nrst: in std_logic;	
+      start: in std_logic;      
       key_i : in std_logic_vector(KEY_SIZE-1 downto 0);	
 			plaintext: in std_logic_vector(DATA_SIZE-1 downto 0); 
-      ciphertext: out std_logic_vector (DATA_SIZE-1 downto 0)
+      ciphertext: out std_logic_vector (DATA_SIZE-1 downto 0);
+      done: out std_logic
     );
 end component;
 
 	signal  s_clk :  std_logic:='0';
-	signal	s_nrst:  std_logic;
+	signal	s_nrst:  std_logic;  
 	signal  s_key_i :  std_logic_vector(KEY_SIZE-1 downto 0);	
 	signal	s_plaintext:  std_logic_vector(DATA_SIZE-1 downto 0) ; 
 	signal  s_ciphertext:  std_logic_vector (DATA_SIZE-1 downto 0);
+  signal  s_start: std_logic:='0';
+  signal  s_done:  std_logic;
 
 begin
-DUT: top port map (s_clk,s_nrst,s_key_i,s_plaintext,s_ciphertext);
+DUT: top port map (s_clk,s_nrst,s_start,s_key_i,s_plaintext,s_ciphertext,s_done);
 
 	process(s_clk)
 	begin 
@@ -39,15 +43,22 @@ DUT: top port map (s_clk,s_nrst,s_key_i,s_plaintext,s_ciphertext);
 
 	process
 	begin
-		s_nrst<='0';
+    s_nrst<='0';
 	 	s_key_i<= x"1b1a1918131211100b0a090803020100";
 	 	s_plaintext<=x"656b696c20646e75";
 		wait for 500 ns;
-		s_nrst<='1';
-    wait for 200 ns;
-    assert s_ciphertext= x"44c8fc20b9dfa07" report" FATAL ERROR: ciphertext_error." severity error; 
-	 	wait for 1000 ns;
-		s_nrst<='0';
+		s_nrst<='1';     
+		s_start<='1';
+    wait for 50 ns;
+		s_start<='0';
+    --assert s_ciphertext= x"44c8fc20b9dfa07" report" FATAL ERROR: ciphertext_error." severity error;	 	
+    wait for 500 ns;
+    s_start<='1';
+		wait for 50 ns;
+		s_start<='0';
+		--assert s_ciphertext= x"44c8fc20b9dfa07" report" FATAL ERROR: ciphertext_error." severity error;
+		wait for 500 ns;
+    
 	 	wait ;
 	end process;
 
